2e6f49278a1eafe2b9fae713c57e60500a4fc41c
Jordan Kay <jkay10100@gmail.com>
2020-11-11 13:18:19 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,121 @@
+// defining the morse code librarys
+
+const morseCode = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  0: "-----",
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  " ": "/",
+};
+
+const alphabet = {
+  "-----": "0",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  "/": " ",
+  "-·-·--": "!",
+  "·-·-·-": ".",
+  "--··--": ",",
+  " ": "    ",
+  "/": " ",
+};
+
+// defining the HTML
+
+const textOutput = document.getElementById("text-output");
+
+window.addEventListener("DOMContentLoaded", translate);
+window.addEventListener("keyup", translate);
+function translate() {
+  const textInput = document.getElementById("text-input").value.toLowerCase();
+
+  // splitting the input words into an array of characters
+
+  textArray = textInput.split("");
+
+  // creating a function to access the morse library
+
+  function libraryAccess(x) {
+    return morseCode[x];
+  }
+
+  // mapping the textArray of characters to said function
+
+  map1 = textArray.map((char) => libraryAccess(char));
+
+  // transfering the array into a string and replacing ALL the commas with a space
+
+  arrayToString = map1.toString().replace(/,/g, " ");
+
+  textOutput.innerHTML = arrayToString;
+}
+console.log("Step 1, get the text:", "Hello");
+console.log("Step 2, splitting the input words into an array of characters:", textArray);
+console.log("Step 3, creating a function to access the morse library:", libraryAccess);
+console.log("Step 4, mapping the textArray of characters to said function:", map1);
+console.log("Step 5, transfering the array into a string and replacing ALL the commas with a space:", arrayToString);
